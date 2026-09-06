21da0bceda6e89acccd42a705173f415fe1c885b
Akshay Devdhare <devdhareakshay2104@gmail.com>
2026-09-06 08:43:10 +0530

    Refactor application message

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,14 +2,17 @@ package com.example;
 
 public class App {
 
+    public static String getMessage() {
+        return "Java DevOps Project";
+    }
+
     public static void main(String[] args) {
 
         System.out.println("=================================");
-        System.out.println(" Java DevOps Project");
-        System.out.println(" Application is running!");
-        System.out.println(" Deployed using Jenkins + Ansible");
+        System.out.println(getMessage());
+        System.out.println("Application is running!");
+        System.out.println("Version 1.0");
         System.out.println("=================================");
 
     }
 }
-
