44f6d0697f896ad22adf0e1651144af0d42f909f
Akshay Devdhare <devdhareakshay2104@gmail.com>
2026-09-06 08:43:42 +0530

    Improve application unit test

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -2,14 +2,14 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
 
     @Test
-    void applicationTest() {
+    void applicationMessageTest() {
 
-        assertTrue(true);
+        assertEquals("Java DevOps Project", App.getMessage());
 
     }
 }
