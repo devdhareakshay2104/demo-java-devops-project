c65938502e428b8bf531f0077fb2bfb80e9a37fe
Akshay Devdhare <devdhareakshay2104@gmail.com>
2026-09-06 08:31:28 +0530

    Add Java application

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Java DevOps Project");
+        System.out.println(" Application is running!");
+        System.out.println(" Deployed using Jenkins + Ansible");
+        System.out.println("=================================");
+
+    }
+}
+
