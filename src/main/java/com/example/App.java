37110e0f9187a10b2422987787dc4e388ade6d29
Akshay Devdhare <devdhareakshay2104@gmail.com>
2026-09-09 04:50:45 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,11 +8,11 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("=================================");
+        System.out.println("================================");
         System.out.println(getMessage());
         System.out.println("Application is running!");
         System.out.println("Version 1.0");
-        System.out.println("=================================");
+        System.out.println("================================");
 
     }
 }
